ff8f0bcddbe2f926770e1de661aedb707c45204f
Frank Tseng <b19930813@gmail.com>
2022-01-01 21:58:38 +0800

    'revise'

--- cp/src/Component/Frame/main.jsx
+++ cp/src/Component/Frame/main.jsx
@@ -67,11 +67,6 @@ export default function MainForm() {
     })
 
     const [rows , setRows]  = React.useState([
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
     ])
 
 
